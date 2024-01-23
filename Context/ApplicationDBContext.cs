b4dcc9aa5e2ff22e8a57a98e79a0edb07585f464
sgtPepper109 <a.r.shaikh1940@gmail.com>
2024-01-23 14:45:05 +0530

    first commit

---
+++ Context/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using ASPNETAuthAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETAuthAPI.Context {
+    public class ApplicationDBContext: DbContext {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options) {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
