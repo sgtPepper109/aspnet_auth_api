32adf30ec55ed3517189c1923cf7f3c830477682
sgtPepper109 <a.r.shaikh1940@gmail.com>
2024-01-24 13:30:17 +0530

    added migrations and controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using ASPNETAuthAPI.Context;
+using ASPNETAuthAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETAuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDBContext _applicationDBContext;
+        public UserController(ApplicationDBContext applicationDBContext) {
+            _applicationDBContext = applicationDBContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObject) {
+            if (userObject == null) return BadRequest();
+            User userReturned = await _applicationDBContext.Users.FirstOrDefaultAsync(x => x.Email == userObject.Email);
+            if (userReturned == null) return NotFound(new {Message = "User Not Found"});
+            if (userReturned.Password != userObject.Password) return BadRequest(new {Message = "Password Mismatch"});
+            return Ok(new {Message = "User Found"});
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObject) {
+            if (userObject == null) return BadRequest();
+            await _applicationDBContext.Users.AddAsync(userObject);
+            await _applicationDBContext.SaveChangesAsync();
+            return Ok (new {Message = "User Registered"});
+        }
+    }
+}
---
+++ Migrations/20240123094115_v1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASPNETAuthAPI.Migrations
+{
+    public partial class v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
